cc1a957e41eb4725aa04e21591f26b3bba95e16b
Adrián Jaime Pastor <36241023+darckball@users.noreply.github.com>
2018-05-15 18:39:23 +0200

    Algunas cosillas

---
+++ Notas/Try/Assets/NewGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NewGame : MonoBehaviour
+{
+
+
+    public void CambioScena(string Activador)
+    {
+        System.IO.StreamReader file = new System.IO.StreamReader(@".\Assets\TXT\Player_Info\PlayerName.txt");
+        if (file.ReadLine() == null)
+            SceneManager.LoadScene(Activador);
+   
+    }
+}
