a74537ee453d1b3426ed41311cb92d9b2336bca1
Adrián Jaime Pastor <36241023+darckball@users.noreply.github.com>
2018-05-19 16:15:43 +0200

    Cambiar nombre

--- Notas/Try/Assets/Name.cs
+++ Notas/Try/Assets/Name.cs
@@ -1,8 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+
 
 public class Name : MonoBehaviour {
+    public InputField named;
 
-   public void GetInput() { }
+      public void GetInput()
+      {
+          StreamWriter nameFile = new StreamWriter(@".\Assets\TXT\Player_Info\PlayerName.txt");
+          nameFile.Write(named.text,false);
+        nameFile.Close();
+      }
+
 }
+
