5714f8fc916d4a162bcfdfdaf28681c80f9735ea
Alejandro Luque Espín <alejandro.luque@enti.com>
2018-05-25 02:42:37 +0200

    Añadida escena de recompensa
    No teno mucha idea de como enfocar la escena, he puesto algunas cosas pero seguramente se cambie.

---
+++ Notas/Try/Assets/Recompensas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recompensas : MonoBehaviour {
+    public Monedero monedero;
+    public int nivelRecompensa;
+    
+    public void SelectTypeOfReward()
+    {
+        switch (nivelRecompensa)
+        {
+            case 0:
+                monedero.monedasNormales += Random.Range(0,101) ;
+                break;
+            case 1:
+                monedero.monedasNormales += Random.Range(0, 201);
+                break;
+            case 2:
+                monedero.monedasNormales += Random.Range(0, 301);
+                break;
+            case 3:
+                monedero.monedasNormales += Random.Range(0, 401);
+                break;
+            case 4:
+                monedero.monedasNormales += Random.Range(0, 501);
+                break;
+        }
+    }
+
+}
